7d65959626b563c5f90ea27a5418bbd3417885ff
maxecharles <max.charles@sydney.edu.au>
2024-05-28 17:11:04 +1000

    change installed amigo branch

--- bunya/sim_io.sh
+++ bunya/sim_io.sh
@@ -21,7 +21,7 @@ source $EBROOTANACONDA3/etc/profile.d/conda.sh
 conda activate dlux
 
 # Print the environemnts
-pip install --upgrade --no-deps --force-reinstall git+ssh://git@github.com/LouisDesdoigts/amigo.git@dev -q
+pip install --upgrade --no-deps --force-reinstall git+ssh://git@github.com/LouisDesdoigts/amigo.git@mc_custom_model -q
 pip install git+https://git@github.com/itroitskaya/dLuxWebbpsf.git@import_fix -q
 pip install git+https://git@github.com/fmartinache/xara.git -q
 
