4b6c0ac76776fd186e1cb977f8b987386b192393
maxecharles <max.charles@sydney.edu.au>
2024-07-30 15:03:21 +1000

    increasing no. of epochs

--- reg_grid.py
+++ reg_grid.py
@@ -14,7 +14,7 @@ from amgio import (
 jax.config.update("jax_enable_x64", True)
 
 ###################################################################################################
-n_epoch = 6
+n_epoch = 80
 
 coeffs = np.logspace(-3, 3, 15)
 
